e1425c36029a6a6bcbfba342d66817f2dc40141e
Sebastian Ayala <sebas.ayala2002@hotmail.com>
2022-08-25 00:33:36 -0500

    avance

--- task3.cpp
+++ task3.cpp
@@ -31,7 +31,7 @@ void task3(){
     static uint8_t keyCounter;
     static bool ledstate = true;
     static bool ledBlink = true;
-    TaskStates currentState;
+    TaskStates currentState = TaskStates::SLOW_MODE;
     TaskStates lastState = TaskStates::SLOW_MODE;
     
     static uint32_t lastTime;
@@ -56,11 +56,13 @@ void task3(){
         keyCounter=0;
         taskState = TaskStates::WAIT_CONFIG;
         lastTime = millis();
+        currentState = TaskStates::WAIT_CONFIG;
+        lastState = TaskStates::SLOW_MODE;
         break;
     }
     case TaskStates::WAIT_CONFIG:           
     {
-        uint32_t currentTime = millis();
+        currentState=TaskStates::WAIT_CONFIG;
         if (buttonEvt.trigger == true)
         {
             buttonEvt.trigger = false;
@@ -70,19 +72,22 @@ void task3(){
                 ledstate=false;
                 if (buttonEvt.whichButton == BUTTONS::BTN_ONE && ledstate==false)
                 {
-                    taskState = TaskStates::SLOW_MODE;
                     currentState = TaskStates::SLOW_MODE;
+                    taskState = TaskStates::SLOW_MODE;
                 }
                 if (buttonEvt.whichButton == BUTTONS::BTN_TWO && ledstate==false)
                 {
-                    taskState=TaskStates::RAPID_MODE;
+                    currentState = TaskStates::SLOW_MODE;
                     keyCounter = 0;
+                    taskState=TaskStates::RAPID_MODE;
+                    
                 }
             }
             if (buttonEvt.whichButton == BUTTONS::BTN_TWO && lastState == TaskStates::SLOW_MODE)
             {
-                taskState=TaskStates::MID_MODE;
                 currentState = TaskStates::MID_MODE;
+                lastState = TaskStates::MID_MODE;
+                taskState=TaskStates::MID_MODE;
             }
             if (buttonEvt.whichButton == BUTTONS::BTN_ONE && lastState == TaskStates::MID_MODE)
             {
@@ -90,27 +95,30 @@ void task3(){
                 ledstate=true;
                 if (buttonEvt.whichButton == BUTTONS::BTN_ONE && ledstate==true)
                 {
-                    taskState = TaskStates::MID_MODE;
                     currentState = TaskStates::MID_MODE;
+                    taskState = TaskStates::MID_MODE;
                 }
                 if (buttonEvt.whichButton == BUTTONS::BTN_TWO && ledstate==true)
                 {
-                    taskState=TaskStates::RAPID_MODE;
+                    currentState = TaskStates::MID_MODE;
                     keyCounter = 0;
+                    taskState=TaskStates::RAPID_MODE;
                 }
             }
             if (buttonEvt.whichButton == BUTTONS::BTN_TWO && lastState == TaskStates::MID_MODE)
             {
-                taskState=TaskStates::SLOW_MODE;
                 currentState = TaskStates::SLOW_MODE;
+                lastState = TaskStates::SLOW_MODE;
+                taskState=TaskStates::SLOW_MODE;
             }
         }
         break;
     }
     case TaskStates::SLOW_MODE:
     {
-        if( (currentTime - lasTime) >= SLOW ){
-                lasTime = currentTime;
+        uint32_t currentTime = millis();
+        if( (currentTime - lastTime) >= SLOW ){
+                lastTime = currentTime;
                 digitalWrite(led, ledBlink);
                 ledBlink = !ledBlink;
             }
@@ -120,8 +128,9 @@ void task3(){
     }
     case TaskStates::MID_MODE:
     {
-         if( (currentTime - lasTime) >= MID ){
-                lasTime = currentTime;
+        uint32_t currentTime = millis();
+         if( (currentTime - lastTime) >= MID ){
+                lastTime = currentTime;
                 digitalWrite(led, ledBlink);
                 ledBlink = !ledBlink;
             }
@@ -131,9 +140,10 @@ void task3(){
     }
     case TaskStates::RAPID_MODE:
     {
+        uint32_t currentTime = millis();
         lastState=TaskStates::RAPID_MODE;
-         if( (currentTime - lasTime) >= FAST ){
-                lasTime = currentTime;
+         if( (currentTime - lastTime) >= FAST ){
+                lastTime = currentTime;
                 digitalWrite(led, ledBlink);
                 ledBlink = !ledBlink;
             }
